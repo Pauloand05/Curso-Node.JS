883186c8e44452fb1894812fb2f377e1dc08a11d
Paulo Andre <pauloandre050619@gmail.com>
2025-05-17 01:53:19 -0300

    Parametros Node.JS

---
+++ IntroducaoExpress/parametros.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World!");
+    }
+);
+
+app.get("/sobre", function(req, res) {
+    res.send("Sobre nós!");
+});
+
+app.get("/contato", function(req, res) {
+    res.send("Contatos!");
+});
+
+app.get("/ola/:cargo/:nome/:cor", function(req, res) {
+    res.send("<h1>Ola " + req.params.nome + ", seu cargo e " + req.params.cargo + "</h1>" + "<h2>Sua cor e " + req.params.cor , "</h2>");
+
+    // Send só pode ser usado uma vez.
+})
+
+app.listen(8081, () => {
+    console.log('Servidor rodando na url http://localhost:8081');
+});
