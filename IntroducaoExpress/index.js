aa86ec649f4300db103897f576c4f16857b77765
Paulo Andre <pauloandre050619@gmail.com>
2025-05-17 02:30:03 -0300

    Exibindo HTML Node.JS

--- IntroducaoExpress/index.js
+++ IntroducaoExpress/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/html/index.html");
     }
 );
 
 app.get("/sobre", function(req, res) {
-    res.send("Sobre nós!");
+    res.sendFile(__dirname + "/html/sobre.html");
 });
 
 app.get("/contato", function(req, res) {
