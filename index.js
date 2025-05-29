54ffa7c791360a11f4ccac00407753cc9dd30a58
Pauloand05 <pauloandre050619@gmail.com>
2025-05-29 02:38:47 -0300

    Listando dados do banco Node.JS

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const app = express();
 const {engine} = require('express-handlebars');
-const Sequelize = require('sequelize');
+const Post = require("./models/Post");
 
 // Config
     // template engine
         app.engine('handlebars', engine({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
-    // Conexão com o banco de dados
-        const sequelize = new Sequelize('connectbank', 'root', 'root', {
-            host: 'localhost',
-            dialect: 'mysql'
-        })
+    // Configurando o body-parser
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false}))
+    
 
     // Rotas
+        app.get("/", (req, res) => {
+            res.render("home")
+        })
+
         app.get('/cad', (req, res) => {
             res.render("fomularios");
         })
 
         app.post('/add', (req, res) => {
-            res.send("Formulário recebido com sucesso!");
+            Post.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo
+            }).then( () => {
+                res.redirect("/")
+            }).catch( (err) => {
+                res.send("Houve um erro: " + err);
+            })
         })
 
 app.listen(8081, () => {
